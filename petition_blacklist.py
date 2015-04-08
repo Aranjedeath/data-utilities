dbc20f2a2e09c352de65d89b7ad50c08c5ffde49
leosquared <leo14358@gmail.com>
2015-04-08 16:44:57 -0400

    petition cleanup tool

---
+++ petition_blacklist.py
@@ -0,0 +1,23 @@
+from blacklist import blacklist
+import csv, sys
+
+infile = csv.reader(open(sys.argv[1], 'rU'))
+headers = infile.next()
+
+clean_data = []
+blacklist_data = []
+
+for row in infile:
+	if blacklist(row[0]) or blacklist(row[1]) or blacklist(row[2], 50) or blacklist(row[3]):
+		blacklist_data.append(row)
+	else:
+		clean_data.append(row)
+
+clean_outfile = csv.writer(open(sys.argv[1] + '_good.csv', 'w'))
+clean_outfile.writerow(headers)
+clean_outfile.writerows(clean_data)
+
+blacklist_outfile = csv.writer(open(sys.argv[1] + '_bad.csv', 'w'))
+blacklist_outfile.writerow(headers)
+blacklist_outfile.writerows(blacklist_data)
+
